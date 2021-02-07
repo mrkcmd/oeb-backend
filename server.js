f9f3b301d838c5cbbcaf9297825c7b5d0612afb4
zwirwat.t <zwirwat.t@motiftech.com>
2021-02-07 23:28:38 +0700

    fixed database

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // database
 const db = require("./app/models");
 const Role = db.role;
-db.sequelize.sync();
+// db.sequelize.sync();
 // force: true will drop the table if it already exists
-// db.sequelize.sync({force: true}).then(() => {
-//   console.log('Drop and Resync Database with { force: true }');
-//   initial();
-// });
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Database with { force: true }');
+  initial();
+});
 
 
 // simple route
